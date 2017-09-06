00885128fd0f0f4f5415226ebec1af684f3a6e0e
amelkisethian <angela.melkisethian@gmail.com>
2017-09-06 04:23:09 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,39 @@
 function basicTeenager(age) {
-
+  if (age > 12 && age < 20) {
+  return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age > 12 && age < 20) {
+  return "You are a teenager!"
+  }
+  else {
+  return "You are not a teenager"// executed if `conditionToTest` is falsey
+ }
 }
 
 function ageChecker(age) {
-
+  if (age > 12 && age < 20) {
+  return "You are a teenager!"
+  }
+  else if (age <= 12) {
+  return "You are a kid" // executed if `conditionToTest` is falsey
+} else {
+  return "You are a grownup"
+ }
 }
 
 function ternaryTeenager(age) {
-
+return (age > 12 && age < 20) ? "You are a teenager": "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch(age) {
+case (age > 12 && age < 20):
+return "You are a teenager";
+break;
+default:
+return "You have an age"
+ }
 }
